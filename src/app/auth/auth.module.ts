57e1e96830087a027b212c984c88a0a8cfbe9ae3
ViktoriiaBilaniuk <vika.bilaniuk@gmail.com>
2017-12-22 18:13:02 +0200

    designed login ang sign-up pages

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {AuthComponent} from './auth.component';
-import {RegistrationComponent} from './registration/registration.component';
 import {LoginComponent} from './login/login.component';
+import {MatButtonModule, MatCheckboxModule,MatInputModule} from '@angular/material';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 
 const routes: Routes = [
   { path: '', component: AuthComponent, children: [
       {path: 'login', component: LoginComponent},
-      {path: 'registration', component: RegistrationComponent}
+      {path: 'sign-up', component: SignUpComponent}
     ]}
 ];
 
@@ -19,12 +20,16 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MatCheckboxModule,
+    MatInputModule,
+    CommonModule,
+    MatButtonModule
   ],
   declarations: [
     AuthComponent,
     LoginComponent,
-    RegistrationComponent
+    SignUpComponent
   ]
 })
 export class AuthModule { }
