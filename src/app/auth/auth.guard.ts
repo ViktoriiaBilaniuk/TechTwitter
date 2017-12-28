d08553f17bdf1246c21c2a5514bdebcaea7694d6
ViktoriiaBilaniuk <vika.bilaniuk@gmail.com>
2017-12-28 10:47:32 +0200

    first commit

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -11,8 +11,8 @@ export class AuthGuard implements CanActivate {
 
   canActivate(): boolean {
      console.log(this.authService.success);
-    //return this.authService.success;
-    return true;
+    return this.authService.success;
+    //return true;
   }
 
 
