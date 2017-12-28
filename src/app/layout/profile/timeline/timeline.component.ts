e122f235e32ebee31b4a40370484af1525313040
ViktoriiaBilaniuk <vika.bilaniuk@gmail.com>
2017-12-28 19:06:47 +0200

    added wall to profile

--- src/app/layout/profile/timeline/timeline.component.ts
+++ src/app/layout/profile/timeline/timeline.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProfileService} from '../../../common/services/profile.service';
+import {PostModel} from '../../../common/models/PostModel';
 
 @Component({
   selector: 'app-timeline',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit {
+  postsOfCurrentUser: PostModel[] = [];
 
-  constructor() { }
+  constructor(public profileService: ProfileService) { }
 
   ngOnInit() {
+    this.getPosts();
   }
 
+  getPosts() {
+    this.profileService.fetchPosts(this.profileService.currentUser.userId)
+      .subscribe((posts) => {
+        this.postsOfCurrentUser = posts.map((post) => post.payload.val());
+        // this.postsOfCurrentUser[0] = posts[0].payload.val();
+        // console.log(posts);
+      });
+  }
 }
