8c54d72e17d3f65a75d6a5c782dd239419499c80
ViktoriiaBilaniuk <vika.bilaniuk@gmail.com>
2018-01-11 15:00:28 +0200

    refactor

--- src/app/layout/users/filter.pipe.ts
+++ src/app/layout/users/filter.pipe.ts
@@ -10,10 +10,6 @@ export class FilterPipe implements PipeTransform {
     if (!searchText) return items;
     searchText = searchText.toLowerCase();
     return items.filter( it => {
-      console.log(it);
-      console.log(it.firstName.concat(' ').concat(it.lastName));
-      console.log(it.firstName.concat(' ').concat(it.lastName).toLowerCase());
-      console.log(it.firstName.concat(' ').concat(it.lastName).toLowerCase().includes(searchText));
       return it.firstName.concat(' ').concat(it.lastName).toLowerCase().includes(searchText);
     });
   }
